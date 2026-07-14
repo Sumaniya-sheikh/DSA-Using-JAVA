e62b76309dc98b897ca98f55c9df845199be0d5c
Sumaniya Sheikh <sumaniyasheikh1234@gmail.com>
2026-07-14 19:55:33 +0000

    sliding window fixed size

--- SlidingWindow.java
+++ SlidingWindow.java
@@ -1,23 +1,25 @@
-// Find the length of the longest subarray whose sum is ≤ target, assuming all array elements are positive
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
-
-class SlidingWindow1 {
+// finding the maximum sum of subarray of size 2
+class SlidingWindow {
+    //  fixed size
     public static void main(String[] args) {
-        int arr[]={2,3,4 ,5,3, 5,6,7,8};
-        
-        int low=0 , maxLen=0 , sum=0 , target =16 , high =0 ;
-        for(;high < arr.length; ){
-            sum = sum + arr[high];
-          
-            while(sum>target){
-                sum -=arr[low];
-                low++;
-            }
-              int curLen = high - low + 1;
-            maxLen = Math.max(maxLen, curLen);
-            high++;
-        }
-        System.out.println(maxLen);
+     int arr[]={2,3,4 ,5,3, 5,6,7,8};
+      int sum=0, maxSum=0;
+      int low =0 , high=1;
+      
+      sum = arr[low]+arr[high];
+        maxSum= sum;
+     
+     while(high< arr.length-1){
+         sum-=arr[low];
+         low++;
+         high++;
+         
+         sum+= arr[high];
+         maxSum= Math.max(maxSum, sum);
+     }
+     System.out.println(maxSum);
     }
 }
+   
